1885114fec2940e79b6a15bc08420512108dd47e
ricahila816-commits <ricahila816@gmail.com>
2026-05-20 22:46:54 +0800

    Create exceptions.py

---
+++ exceptions.py
@@ -0,0 +1,5 @@
+class BookAlreadyExistsError(Exception): pass
+class InvalidInputError(Exception): pass
+class BookNotFoundError(Exception): pass
+class MemberNotFoundError(Exception): pass
+class BookUnavailableError(Exception): pass
